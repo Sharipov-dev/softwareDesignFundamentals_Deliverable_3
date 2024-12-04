efc2c3af7058d78c051213489d29cab7b62aa995
Arthur Sharipov <sharipov@sheridancollege.ca>
2024-12-04 14:47:32 -0500

    Project initialization

---
+++ src/ca/sheridancollege/project/cards/FrenchCard.java
@@ -0,0 +1,19 @@
+package ca.sheridancollege.project.cards;
+
+import ca.sheridancollege.project.cards.Card;
+
+public class FrenchCard extends Card {
+
+    private Suit suit;
+    private Value value;
+
+    /**
+     * Students should implement this method for their specific children classes
+     *
+     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
+     */
+    @Override
+    public String toString() {
+        return null;
+    }
+}
