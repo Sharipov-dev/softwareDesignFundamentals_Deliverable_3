a33f309454986467d650cb6318304d8bef2b9f26
elandrew23 <elandrew@sheridancollege.ca>
2024-12-05 05:15:26 -0500

    Slight tweak to how players are numbered when hands are set.

--- src/ca/sheridancollege/project/SetHand.java
+++ src/ca/sheridancollege/project/SetHand.java
@@ -33,7 +33,7 @@ public class SetHand {
         List<List<Card>> dealtCards = new ArrayList<>();
 
         for (int i = 0; i < players; i++) {
-            System.out.println(String.format("Setting hand for %s player", i));
+            System.out.println(String.format("Setting hand for " + (i + 1) + " player", i));
             List<Card> playerHand = new ArrayList<>(cards.getCards().subList(0, cardNumberPerPlayer));
             cards.getCards().subList(0, cardNumberPerPlayer).clear();
             dealtCards.add(playerHand);
